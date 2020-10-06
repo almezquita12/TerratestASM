876400303164828571a75d7e1ab5a8036ac46e53
almezquita12 <72026371+almezquita12@users.noreply.github.com>
2020-10-06 13:08:44 +0200

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "secret_ids" {
-  description = "Secret id list"
-  value       = aws_secretsmanager_secret.asm.*.id
-}
-output "secret_arns" {
-  description = "Secret arn list"
-  value       = aws_secretsmanager_secret.asm.*.arn
-}
